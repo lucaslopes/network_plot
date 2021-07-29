07742407a8edadd843ec8722679e833666719639
Lucas Lopes <lucaslopesf2@gmail.com>
2021-07-29 19:27:13 -0300

    plot a network

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from dash import Dash
-from dash_html_components import H1
+import dash_cytoscape as cyto
+import dash_html_components as html
 
-app = Dash(__name__)
+app = Dash('Network Plotter')
 server = app.server
 
-app.layout = H1('Hello World.')
+app.layout = html.Div([
+    cyto.Cytoscape(
+        id='cytoscape-two-nodes',
+        layout={'name': 'preset'},
+        style={'width': '100%', 'height': '400px'},
+        elements=[
+            {'data': {'id': 'one', 'label': 'Node 1'}, 'position': {'x': 75, 'y': 75}},
+            {'data': {'id': 'two', 'label': 'Node 2'}, 'position': {'x': 200, 'y': 200}},
+            {'data': {'source': 'one', 'target': 'two'}}
+        ]
+    )
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
